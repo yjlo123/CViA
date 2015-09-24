e0ecdbc6f852f17c851225ce4c620ca5f10ad016
Nguyen Nguyen <nguyennts93@gmail.com>
2015-09-25 07:40:51 +0800

    Initialize Flask

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-__author__ = 'siwei'
+from flask import Flask
 
-print "Hello World\n"
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888)
