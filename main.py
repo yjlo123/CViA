ca3a6df58ec7b06b08b8f6f427c37c2d898adf4a
Nguyen Nguyen <nguyennts93@gmail.com>
2015-09-25 08:32:44 +0800

    Sample template

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=['GET'])
+def index():
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888)
