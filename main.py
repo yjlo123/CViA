fd01c0d5d12886738cafc6718f09ce230955b474
Nguyen Nguyen <nguyennts93@gmail.com>
2015-09-26 01:11:49 +0800

    Upload and index template

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route("/upload", methods=['GET'])
+def upload():
+    return render_template('upload.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888)
