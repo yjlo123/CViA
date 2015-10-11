0e31359a19e31cde2b9ecedcb2c280e1ef848a90
Nguyen Nguyen <nguyennts93@gmail.com>
2015-10-11 22:55:22 +0800

    Display converted text after upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, url_for, redirect, request
 from werkzeug import secure_filename
+from doc_converter import DocConverter
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'cv/'
@@ -15,16 +16,23 @@ def index():
 
 @app.route("/upload", methods=['GET', 'POST'])
 def upload():
-    print request.method
+    # POST
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             print "Uploaded file", filename
-            print os.path.abspath(__file__)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template('cv.html', filename=filename)
+
+            full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(full_path)
+            print "Stored file", filename
+
+            text = DocConverter().documentToText(full_path)
+            return render_template('cv.html', filename=filename, text=text)
+
         return render_template('cv.html', filename=None)
+
+    # GET
     return render_template('upload.html')
 
 if __name__ == "__main__":
