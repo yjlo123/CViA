edcc60a66c61b84db868ccfd1f5f4481c38f2217
Nguyen Nguyen <nguyennts93@gmail.com>
2015-10-11 22:32:35 +0800

    Store uploaded files

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, url_for, redirect, request
+from werkzeug import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'cv/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'doc', 'docx'])
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route("/upload", methods=['GET'])
+@app.route("/upload", methods=['GET', 'POST'])
 def upload():
+    print request.method
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print "Uploaded file", filename
+            print os.path.abspath(__file__)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('cv.html', filename=filename)
+        return render_template('cv.html', filename=None)
     return render_template('upload.html')
 
 if __name__ == "__main__":
